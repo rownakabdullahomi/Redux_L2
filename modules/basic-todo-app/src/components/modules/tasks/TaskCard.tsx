9acfe13adf83038a45110b1d433431f7734d7e99
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-03 01:54:14 +0600

    delete task done

--- modules/basic-todo-app/src/components/modules/tasks/TaskCard.tsx
+++ modules/basic-todo-app/src/components/modules/tasks/TaskCard.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
+import { useAppDispatch } from "@/redux/features/hooks";
+import {
+  deleteTask,
+  toggleCompleteState,
+} from "@/redux/features/task/taskSlice";
 import type { ITask } from "@/types";
 import { Trash2 } from "lucide-react";
 
@@ -9,6 +14,8 @@ interface IProps {
 }
 
 const TaskCard = ({ task }: IProps) => {
+  const dispatch = useAppDispatch();
+
   return (
     <div className="border px-5 py-3 rounded-md">
       <div className="flex justify-between items-center">
@@ -20,13 +27,17 @@ const TaskCard = ({ task }: IProps) => {
               "bg-red-500": task.priority === "high",
             })}
           ></div>
-          <h1>{task.title}</h1>
+          <h1 className={cn({"line-through" : task.isCompleted})}>{task.title}</h1>
         </div>
         <div className="flex items-center gap-3">
-          <Button variant={"link"} className="p-0 text-red-500">
+          <Button
+            onClick={() => dispatch(deleteTask(task.id))}
+            variant={"link"}
+            className="p-0 text-red-500"
+          >
             <Trash2 />
           </Button>
-          <Checkbox />
+          <Checkbox checked={task.isCompleted} onClick={() => dispatch(toggleCompleteState(task.id))} />
         </div>
       </div>
       <p className="mt-5">{task.description}</p>
--- modules/basic-todo-app/src/redux/features/task/taskSlice.ts
+++ modules/basic-todo-app/src/redux/features/task/taskSlice.ts
@@ -2,7 +2,6 @@ import type { RootState } from "@/redux/store";
 import type { ITask } from "@/types";
 import { createSlice, nanoid, type PayloadAction } from "@reduxjs/toolkit";
 
-
 interface IInitialState {
   tasks: ITask[];
   filter: "all" | "high" | "medium" | "low";
@@ -10,22 +9,22 @@ interface IInitialState {
 
 const initialState: IInitialState = {
   tasks: [
-    // {
-    //   id: "guihuykjhk",
-    //   title: "Init Frontend",
-    //   description: "Create home page and routing",
-    //   dueDate: "2025-11",
-    //   isCompleted: false,
-    //   priority: "high",
-    // },
-    // {
-    //   id: "g80980uykjhk",
-    //   title: "Init Github",
-    //   description: "Create github repo and push",
-    //   dueDate: "2025-11",
-    //   isCompleted: false,
-    //   priority: "medium",
-    // },
+    {
+      id: "guihuykjhk",
+      title: "Init Frontend",
+      description: "Create home page and routing",
+      dueDate: "2025-11",
+      isCompleted: false,
+      priority: "high",
+    },
+    {
+      id: "g80980uykjhk",
+      title: "Init Github",
+      description: "Create github repo and push",
+      dueDate: "2025-11",
+      isCompleted: false,
+      priority: "medium",
+    },
   ],
   filter: "all",
 };
@@ -48,6 +47,17 @@ const taskSlice = createSlice({
       const taskData = createTask(action.payload);
       state.tasks.push(taskData);
     },
+    toggleCompleteState: (state, action: PayloadAction<string>) => {
+      state.tasks.forEach((task) =>
+        task.id === action.payload
+          ? (task.isCompleted = !task.isCompleted)
+          : task
+      );
+    },
+
+    deleteTask: (state, action: PayloadAction<string>)=>{
+      state.tasks =  state.tasks.filter(task => task.id !== action.payload)
+    }
   },
 });
 
@@ -58,5 +68,5 @@ export const selectFilter = (state: RootState) => {
   return state.taskR.filter;
 };
 
-export const { addTask } = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
 export default taskSlice.reducer;
