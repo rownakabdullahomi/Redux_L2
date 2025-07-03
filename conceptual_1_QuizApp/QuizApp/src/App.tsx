90be3b1df9d539789a81c57cc30ba5cdceba60a8
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-03 17:24:35 +0600

    add quiz form added

--- conceptual_1_QuizApp/QuizApp/src/App.tsx
+++ conceptual_1_QuizApp/QuizApp/src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import AddQuiz from "./pages/home/AddQuiz";
 import Question from "./pages/home/Question";
 import QuizSummary from "./pages/home/QuizSummary";
 import { useAppSelector } from "./redux/hooks";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="">
       <h1 className="text-center text-7xl my-12">Quiz App</h1>
+      <AddQuiz />
       {!quizComplete ? <Question /> : <QuizSummary />}
     </div>
   );
